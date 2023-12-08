67c6ea4ed8efe380589d4f07236e7e74acf5f5c8
caraxesmsc <mohamedongo21@gmail.com>
2023-12-08 16:59:54 +0200

    Fixed reg and inputs

--- mian.v
+++ mian.v
@@ -1,14 +1,15 @@
 module ATMmodule(
+    input  [1:0] language,// 00 is default ---- 01 is English ----- 10 is German
+    input  [2:0] service,// 000 is default ---- 001 deposit ---- 010 withdraw ---- 011 checkBalance ---- 100 101 111
+    input  [4:0] amount,// money to deposit or withdraw
+    input  [7:0] cardno,
+    input  [3:0] pin,
+    input  anotherServiceBit,// 0 is default means no another service if changed to 1 then go back to serviceSate
     input clk,
     input [3:0]  correctPin,
     output reg [4:0] balance // money in your bank acc.
 );
-reg [1:0] language;// 00 is default ---- 01 is English ----- 10 is German
-reg [2:0] service;// 000 is default ---- 001 deposit ---- 010 withdraw ---- 011 checkBalance ---- 100 101 111
-reg [4:0] amount;// money to deposit or withdraw
-reg [7:0] cardno;
-reg [3:0] pin;
-reg anotherServiceBit;// 0 is default means no another service if changed to 1 then go back to serviceSate
+
     reg [31:0] timerCounter;
 
     // States definition
@@ -25,20 +26,23 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
     // State and nextState registers
     reg [3:0] state, nextState;
 
-    // Initialization block
-    // initial begin
-    //     // Initial values for the inputs
-    //     language = 2'b00;
-    //     service = 3'b000;
-    //     amount = 5'b00000;
-    //     cardno = 8'b00000000;
-    //     pin = 4'b0000;
-    //     anotherServiceBit = 0;
-    // end
-
+    reg [1:0] language_reg;
+    reg [2:0] service_reg;
+    reg [4:0] amount_reg;
+    reg [7:0] cardno_reg;
+    reg [3:0] pin_reg;
+    reg anotherServiceBit_reg;
 
     always @(posedge clk) 
     begin
+        // Assign input values to reg variables
+        language_reg = language;
+        service_reg = service;
+        amount_reg = amount;
+        cardno_reg = cardno;
+        pin_reg = pin;
+        anotherServiceBit_reg = anotherServiceBit;
+
         // Sequential logic: State transition on positive edge of the clock
         if(state != nextState)
             timerCounter = 0;
@@ -46,6 +50,7 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
     end
 
     always @(*) begin
+        
         // Combinational logic: Determines the next state based on the current state and inputs
         nextState = state; // Default nextState is the current state
         case(state)
@@ -53,7 +58,7 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
             idleState:  
                 begin
                     // Transition to languageState if card number is present
-                    if(cardno != 8'b0 )
+                    if(cardno_reg != 8'b0 )
                         nextState = languageState;
                     else
                         nextState = state;
@@ -65,13 +70,13 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                     timerCounter = 0;
 
                     // Check selected language
-                    if(language == 2'b01)
+                    if(language_reg == 2'b01)
                     begin
                         // Display and transition to pinState after 2 clock cycles
                         // $display("English Language is chosen");
                         nextState = pinState;
                     end
-                    else if (language == 2'b10)
+                    else if (language_reg == 2'b10)
                     begin
                         // Display and transition to pinState after 2 clock cycles
                         // $display("German Language is chosen");
@@ -88,12 +93,12 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                             // Display, reset to idleState, and clear inputs
                             // $display("No Action taken");
                             nextState = idleState;
-                            language = 2'b00;
-                            service = 3'b000;
-                            amount = 5'b00000;
-                            cardno = 8'b00000000;
-                            pin = 4'b0000;
-                            anotherServiceBit = 0;
+                            language_reg = 2'b00;
+                            service_reg = 3'b000;
+                            amount_reg = 5'b00000;
+                            cardno_reg = 8'b00000000;
+                            pin_reg = 4'b0000;
+                            anotherServiceBit_reg = 0;
                         end
                     end
                 end
@@ -104,7 +109,7 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                     timerCounter = 0;
 
                     // Check if the entered PIN is correct
-                    if(pin == correctPin)
+                    if(pin_reg == correctPin)
                         nextState = serviceState;
                     else
                     begin
@@ -117,12 +122,12 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                             // Display, reset to idleState, and clear inputs
                             // $display("No Action taken");
                             nextState = idleState;
-                            language = 2'b00;
-                            service = 3'b000;
-                            amount = 5'b00000;
-                            cardno = 8'b00000000;
-                            pin = 4'b0000;
-                            anotherServiceBit = 0;
+                            language_reg = 2'b00;
+                            service_reg = 3'b000;
+                            amount_reg = 5'b00000;
+                            cardno_reg = 8'b00000000;
+                            pin_reg = 4'b0000;
+                            anotherServiceBit_reg = 0;
                         end
                     end
                 end
@@ -133,9 +138,9 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                     timerCounter = 0;
 
                     // Check if a service is selected
-                    if(service != 3'b000)
+                    if(service_reg != 3'b000)
                     begin
-                        case(service)
+                        case(service_reg)
                             //3'b000: nextState = state;  
                             3'b001: nextState = depositState;
                             3'b010: nextState = withdrawState;
@@ -154,12 +159,12 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                             // Display, reset to idleState, and clear inputs
                             // $display("No Action taken");
                             nextState = idleState;
-                            language = 2'b00;
-                            service = 3'b000;
-                            amount = 5'b00000;
-                            cardno = 8'b00000000;
-                            pin = 4'b0000;
-                            anotherServiceBit = 0;
+                            language_reg = 2'b00;
+                            service_reg = 3'b000;
+                            amount_reg = 5'b00000;
+                            cardno_reg = 8'b00000000;
+                            pin_reg = 4'b0000;
+                            anotherServiceBit_reg = 0;
                         end
                     end
                 end
@@ -167,7 +172,7 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
             depositState:
                 begin
                     // Check if the deposit amount is valid
-                    if(amount != 5'b0)
+                    if(amount_reg != 5'b0)
                     begin
                         // if(amount < 0)
                         // begin
@@ -179,7 +184,7 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                         begin
                             // Display, update balance, and transition to anotherServiceState
                             // $display("%d Amount Deposited into your Account", amount);
-                            balance = balance + amount;
+                            balance = balance + amount_reg;
                             nextState = anotherServiceState;
                         end
                     end
@@ -194,12 +199,12 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                             // Display, reset to idleState, and clear inputs
                             // $display("No Action taken");
                             nextState = idleState;
-                            language = 2'b00;
-                            service = 3'b000;
-                            amount = 5'b00000;
-                            cardno = 8'b00000000;
-                            pin = 4'b0000;
-                            anotherServiceBit = 0;
+                            language_reg = 2'b00;
+                            service_reg = 3'b000;
+                            amount_reg = 5'b00000;
+                            cardno_reg = 8'b00000000;
+                            pin_reg = 4'b0000;
+                            anotherServiceBit_reg = 0;
                         end
                     end
                 end
@@ -207,15 +212,15 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
             withdrawState:
                 begin
                     // Check if the withdraw amount is valid
-                    if(amount != 5'b0)
+                    if(amount_reg != 5'b0)
                     begin
-                        if(amount >= balance)
+                        if(amount_reg >= balance)
                             // If withdraw amount exceeds balance, transition to idleState
                             nextState = idleState;
                         else
                         begin
                             // Update balance and transition to anotherServiceState
-                            balance = balance - amount;
+                            balance = balance - amount_reg;
                             nextState = anotherServiceState;
                         end
                     end
@@ -230,12 +235,12 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                             // Display, reset to idleState, and clear inputs
                             // $display("No Action taken");
                             nextState = idleState;
-                            language = 2'b00;
-                            service = 3'b000;
-                            amount = 5'b00000;
-                            cardno = 8'b00000000;
-                            pin = 4'b0000;
-                            anotherServiceBit = 0;
+                            language_reg = 2'b00;
+                            service_reg = 3'b000;
+                            amount_reg = 5'b00000;
+                            cardno_reg = 8'b00000000;
+                            pin_reg = 4'b0000;
+                            anotherServiceBit_reg = 0;
                         end
                     end
                 end
@@ -244,7 +249,7 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                 begin
                     // Display balance and transition to anotherServiceState
                     // $display("Your Balance is : %d ", balance);
-                    nextState = anotherServiceState;
+                    nextState = anotherServiceState_reg;
                 end
 
             anotherServiceState:
@@ -253,7 +258,7 @@ reg anotherServiceBit;// 0 is default means no another service if changed to 1 t
                     // $display("Do you want to make another service");
 
                     // Transition to serviceState if anotherServiceBit is set, else idleState
-                    if(anotherServiceBit)
+                    if(anotherServiceBit_reg)
                         nextState = serviceState;
                     else
                         nextState = idleState;
